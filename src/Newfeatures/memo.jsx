e2f39512f6bcbe6140a4757f0bf57e1f2662ad9e
likanghua <lkh19890913@163.com>
2020-09-09 22:43:12 +0800

    docs: add memo docs

--- src/Newfeatures/memo.jsx
+++ src/Newfeatures/memo.jsx
@@ -9,13 +9,21 @@ class Baz extends React.PureComponent {
   }
 }
 
-const Bar = memo(function (props) {
-  const { n } = props
-  console.log('function Component Bar render')
-  return (
-    <div>{n}</div>
-  )
-})
+const Bar = memo(
+  function (props) {
+    const { n } = props
+    console.log('function Component Bar render')
+    return (
+      <div>{n}</div>
+    )
+  },
+  (prev, next) => {
+    console.log('prev',prev)
+    console.log('next',next)
+    console.log('prev.n === next.n',prev.n === next.n)
+    return prev.n === next.n              // 返回值是false 执行第一个function
+  }
+)
 
 // function Bar () {
 //   console.log('function Component Bar render')
@@ -28,16 +36,18 @@ class MemoComponent extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      n: 34
+      n: 34,
+      m: 23
     }
   }
   render () {
-    const { n } = this.state
+    const { n,m } = this.state
     return (
       <React.Fragment>
-        <button onClick={()=> this.setState({ n: n+1 })}>Btn</button>
-        <Baz />
-        <Bar />
+        <button onClick={()=> this.setState({ n: n+1 })}>Btn:{n}</button>
+        <button onClick={()=> this.setState({ m: m+1 })}>Btn:{m}</button>
+        <Baz n={n}/>
+        <Bar n={n}/>
       </React.Fragment>
     )
   }
